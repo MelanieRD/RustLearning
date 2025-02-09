e362a197f44ab1015eeacacb1c6ebd4fa3de5e3b
Melanie <melaniehernandez153@hotmail.com>
2025-02-09 07:30:46 -0400

    first steps with rust

---
+++ rustHelloWorld/hello.rs
@@ -0,0 +1,24 @@
+fn main(){
+    println!("Hello, World!");
+}
+
+
+
+
+/* 
+    To compile and run the program, follow the steps below:
+
+- Compile program:
+    rustc hello.rs 
+
+- Run program:
+    ./hello
+
+    --------------------- Or using Cargo package manager ---------------------
+
+    cargo new nameProyect  <--- it creates a new project
+
+    go to the terminal of the new folder created by cargo and then run:
+        cargo run <--- it compiles and runs the program
+
+*/
